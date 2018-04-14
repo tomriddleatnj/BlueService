8826acedabe7b2c430b90fee326295551d7ac5fc
Tom Ye <beag.ye@gmail.com>
2018-04-14 10:39:44 +0800

    seafile installed

---
+++ seafile/conf/seahub_settings.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+SECRET_KEY = "er6s)_gxea7s3r05jh)qib!7ws+6ibpf8q10+%&ybs^njze9uy"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'seahub-db',
+        'USER': 'seafile',
+        'PASSWORD': '917',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
+    }
+}
+
